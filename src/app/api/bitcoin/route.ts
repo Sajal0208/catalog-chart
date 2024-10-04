618685ad24422fc8c4a48591000621de290e7686
Sajal0208 <sajaldewangan@gmail.com>
2024-10-05 02:57:35 +0530

    Added API route

---
+++ src/app/api/bitcoin/route.ts
@@ -0,0 +1,16 @@
+import { processDataForInterval } from "@/utils/data-processing";
+import data from "@/utils/data/bitcoin-market-price.json";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const timeInterval = searchParams.get("timeInterval") || "1d";
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const processedData = await processDataForInterval(
+    data["market-price"],
+    timeInterval
+  );
+
+  return NextResponse.json(processedData);
+}
